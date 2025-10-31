4052d06e18f98e47dcf102f1063bd96d7eb3885e
Michael Fay <52039648+emzmwf@users.noreply.github.com>
2025-10-31 15:40:05 +0000

    Refactor folder handling and improve comments
    Include date of this version, add comments, remove test lines

--- GMS_To_Blender/Make128x128Surface_ForExport2Blender.py
+++ GMS_To_Blender/Make128x128Surface_ForExport2Blender.py
@@ -1,10 +1,13 @@
 # Get front dm image, extract square region
 # and bin down to 128x128
+# MWF for UoN October 2025
 
 import DigitalMicrograph as DM
 import numpy as np
 
-
+# Documentation for using the GMS UI from python is patchy
+# so the alternative is to start using tkinter, or just ask for a 
+# string and rely on the user realising we want forward slashes not back
 def DefineFolderLocation():
     globalTags = DM.GetPersistentTagGroup()
     folder = input( 'Please paste directory folder here FORWARD SLASHES:' )
@@ -12,6 +15,7 @@ def DefineFolderLocation():
     globalTags['DM2Blender Folder'] = folder
     return folder
 
+# See if the folder location is in global tags
 def GetFolderLocation():
     globalTags = DM.GetPersistentTagGroup()
     try:
@@ -21,15 +25,15 @@ def GetFolderLocation():
         folder = DefineFolderLocation()
     return folder
 
-
+# Rebin the image to the desired size
 def rebin(arr, new_shape):
     """Rebin 2D array arr to shape new_shape by averaging."""
     shape = (new_shape[0], arr.shape[0] // new_shape[0],
              new_shape[1], arr.shape[1] // new_shape[1])
     return arr.reshape(shape).mean(-1).mean(1)
 
+# Get the front image or the crop within it
 def GetCrop():
-
     dmImg = DM.GetFrontImage() # Get reference to front most image
     dmImgData = dmImg.GetNumArray() # Get NumpyArray to image data
 
@@ -41,7 +45,7 @@ def GetCrop():
     binimg = rebin(newimg, (outsize,outsize))
     return binimg
     
-
+#Convert the GMS data into a numpy array
 def makesurface(z):
     
     x = np.linspace(0,128, 128)
@@ -50,15 +54,12 @@ def makesurface(z):
     print(x.shape)
     print(y.shape)
     print(z.shape)
-    #print(GetFolderLocation())
     try:
         folder = str(GetFolderLocation())+'/NPArrays128.npz'
     except:
         print("Folder badly defined")
         folder = DefineFolderLocation()
         
-    #folder = 'X:/MW Fay/Scripts/Image Processing/BlenderRender'+'/NPArrays128.npz'
-    #print(folder)
     np.savez(folder,x,y,z)
    
 
