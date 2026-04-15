ccd5d1251e03eb591ca45c940dfee74ef93adc6a
Michael Fay <52039648+emzmwf@users.noreply.github.com>
2026-04-15 10:21:57 +0100

    Update InterpolateOverAnomaly.py
    Fix error in interpolation routine
    Leave original file unchanged
    Correct name output for interpolated file

--- InterpolateOverAnomaly.py
+++ InterpolateOverAnomaly.py
@@ -13,67 +13,29 @@ print("/n ======== /n")
 image_0 = DM.GetFrontImage()
 im_name = image_0.GetName()
 
-NumArr = image_0.GetNumArray() # Get NumpyArray to image data
-
-#nDim = image_0.GetNumDimensions()
-#numpy_array = image_0.GetNumArray()*image_0.GetIntensityScale() 
-#i_unit = image_0.GetIntensityUnitString()	# get y axis units
-#i_origin = image_0.GetIntensityOrigin()	# y axis origin
-
-
-# Get the anomoly range from selected area
-# get range?
-# val, val2= roi.GetRange()
+NumArr = np.copy(image_0.GetNumArray()) # Get NumpyArray to image data
 
 # Or just ask the user
 # Prompt the user for positions
 ok, SPos = DM.GetNumber( 'Enter index to start interpolation over.', 30 ) 
 ok, EPos = DM.GetNumber( 'Enter index to end interpolation over.', 50 )
 
-#spectrumBefore = NumArr[int(0),int(SPos)]
-#spectrumAnomoly = NumArr[int(SPos),int(EPos)]
 spectrumOut = NumArr[:,:]
 
-#spectrumBefore = NumArr[(NumArr>=1)*(NumArr<33)] # no, this is values
 spectrumBefore = NumArr[:, 0:int(SPos-1)]
-#spectrumBefore = NumArr[0:int(SPos-1), 0:int(SPos-1)]
 spectrumDuring = NumArr[:, int(SPos):int(EPos)]
-#spectrumDuring = NumArr[int(SPos):int(EPos), int(SPos):int(EPos)]
 
 #so the width is the NumArr.size / length
 MaxIndex = NumArr.size / len(NumArr)
 print("Max index is ")
 print(MaxIndex)
 
-#SPEC1 = DM.CreateImage(spectrumBefore.copy(order='C')).ShowImage()
-#testImg = DM.GetFrontImage()
-#testImg.SetName("Before")
-#img_disp = testImg.GetImageDisplay(0)
-#img_disp.ChangeDisplayType(3)   #1 is image, 2 is 3d plot, 3 is a line plot
-
-#SPEC2 = DM.CreateImage(spectrumDuring.copy(order='C')).ShowImage()
-#testImg2 = DM.GetFrontImage()
-#testImg2.SetName("During")
-#img_disp2 = testImg2.GetImageDisplay(0)
-#img_disp2.ChangeDisplayType(3)   #1 is image, 2 is 3d plot, 3 is a line plot
-
-#print(spectrumDuring)
-#print(spectrumDuring.shape)    # get array shape, should be 2 by number of inxed points
-#print(spectrumDuring[:,1])  # give values of second index point (zero is first)
-#print(spectrumDuring[1,:])  # gives values of y axis shift, x shift is first array
-
-#a = np.array([[1, 2, 3], [3, 4, 5]])
-#b = np.array([[5, 6], [6, 8]])
-#specOut = np.concatenate((a,b), axis=1)
-#print(specOut)
-
 spectrumAft = np.zeros([2,1])
-#spectrumAft = np.array([])
 
 #Check if there is any after
 IsAft = 0
 if (EPos < MaxIndex):
-    print("There is an after")
+    print("End isn't the last point")
     IsAft = 1
     spectrumAft = NumArr[:, int(EPos+1):int(MaxIndex)]
 #    SPEC3 = DM.CreateImage(spectrumAft.copy(order='C')).ShowImage()
@@ -89,7 +51,6 @@ print(spectrumAft.size)
 #Set to Zero or interpolate?
 #Choice = "zero"
 Choice = "int"
-
 
 # def - remove anomoly set to zero
 def anomolyzero(spectrumAnomoly):
@@ -116,11 +77,8 @@ def anomolyinterpolate(specA, specB, **kwargs):
             #need AL-1 index
             fxval = specA[0][AL-1]
             fyval = specA[1][AL-1]
-            #print(fxval)
-            #print(fyval)
             specB[0,:] = fxval
             specB[1,:] = fyval
-            #specB.fill(fxval)
         specOut = np.concatenate((specA,specB), axis=1)
     else:
         if Choice == "zero":
@@ -138,8 +96,12 @@ def anomolyinterpolate(specA, specB, **kwargs):
             fyvalC = specC[1][0]
             #Probably want to use scipy to interpolate this later, for now just quick and dirty
             CL = specC.shape[1]
-            for i in range(CL-1):
+            print(np.shape(specB))
+            print(CL)
+            #for i in range(CL):
+            for i in range(np.shape(specB)[1]):
                 fxval = (fxvalA*(1-(i/CL))+fxvalC*(i/CL))
+                print("i is "+str(i))
                 specB[0,i] = fxval
                 fyval = (fyvalA*(1-(i/CL))+fyvalC*(i/CL))
                 specB[1,i] = fyval
@@ -156,20 +118,9 @@ else:
 
 SPEC3 = DM.CreateImage(specOut.copy(order='C')).ShowImage()
 testImg3 = DM.GetFrontImage()
-testImg3.SetName("Int")
+
+testImg3.SetName(im_name+"_Interpolated")
 img_disp3 = testImg3.GetImageDisplay(0)
 img_disp3.ChangeDisplayType(3)   #1 is image, 2 is 3d plot, 3 is a line plot
-
-
-
-#spectrumDuring = anomolyzero(spectrumDuring)
-
-
-
-#b = 0
-#if IsAft > b:
-#  specOut = np.concatenate((spectrumBefore,spectrumDuring, spectrumAft), axis=1)
-#else:
-#  specOut = np.concatenate((spectrumBefore,spectrumDuring), axis=1)
 
 image_0.UpdateImage()
